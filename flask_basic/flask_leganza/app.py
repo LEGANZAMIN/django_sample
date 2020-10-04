985eb2756f8e0f367cdb0708848a1b9574d10dcf
LeeSungmin <lsmin01@gmail.com>
2020-10-04 09:45:48 +0900

    flask

---
+++ flask_basic/flask_leganza/app.py
@@ -0,0 +1,17 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+dbfile = os.path.join(basedir, 'db.sqlite')
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + dbfile
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+
+@app.route('/')
+def hello():
+    return 'Hello world'
