e067966769261f10650062e6ee5954fe201aea83
LeeSungmin <lsmin01@gmail.com>
2020-10-05 22:42:14 +0900

    flask-wtf validation

--- flask_basic/flask_leganza/app.py
+++ flask_basic/flask_leganza/app.py
@@ -1,17 +1,66 @@
 import os
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flask import request
+from flask import redirect
+from flask import render_template
+from models import db
+from flask_wtf.csrf import CSRFProtect
+from forms import RegisterForm
 
-basedir = os.path.abspath(os.path.dirname(__file__))
-dbfile = os.path.join(basedir, 'db.sqlite')
+from models import Fcuser
+
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + dbfile
-app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy(app)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+
+    form = RegisterForm()
+    if form.validate_on_submit():
+
+        # if request.method == 'POST':
+
+        #     # 회원정보 설정
+        #     userid = request.form.get('userid')
+        #     username = request.form.get('username')
+        #     password = request.form.get('password')
+        #     re_password = request.form.get('re-password')
+
+        # # validation
+        # if (userid and username and password and re_password) and password == re_password:
+
+        fcuser = Fcuser()
+        fcuser.userid = form.data.get('userid')
+        fcuser.username = form.data.get('username')
+        fcuser.password = form.data.get('password')
+
+        db.session.add(fcuser)
+        db.session.commit()
+        print('Success!!!')
+
+        return redirect('/')
+
+    return render_template('register.html', form=form)
 
 
 @app.route('/')
 def hello():
-    return 'Hello world'
+    return render_template('hello.html')
+
+
+if __name__ == "__main__":
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    dbfile = os.path.join(basedir, 'db.sqlite')
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + dbfile
+    app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SECRET_KEY'] = 'ddffggghhhjkllfghhj'
+
+    csrf = CSRFProtect()
+    csrf.init_app(app)
+
+    db.init_app(app)
+    db.app = app
+    db.create_all()
+    app.run(host='127.0.0.1', port=5000, debug=True)
