5ae27a6b8a6891f50afb21976acc44889ec6781b
LeeSungmin <lsmin01@gmail.com>
2020-10-05 23:43:51 +0900

    login logout

--- flask_basic/flask_leganza/app.py
+++ flask_basic/flask_leganza/app.py
@@ -1,15 +1,34 @@
 import os
 from flask import Flask
 from flask import request
+from flask import session
 from flask import redirect
 from flask import render_template
 from models import db
 from flask_wtf.csrf import CSRFProtect
-from forms import RegisterForm
+from forms import RegisterForm, LoginForm
 
 from models import Fcuser
 
 app = Flask(__name__)
+
+
+@app.route('/logout', methods=['GET'])
+def logout():
+    session.pop('userid', None)
+    return redirect('/')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        # Login
+        session['userid'] = form.data.get('userid')
+
+        return redirect('/')
+
+    return render_template('login.html', form=form)
 
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -45,7 +64,8 @@ def register():
 
 @app.route('/')
 def hello():
-    return render_template('hello.html')
+    userid = session.get('userid', None)
+    return render_template('hello.html', userid=userid)
 
 
 if __name__ == "__main__":
